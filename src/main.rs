24b610eca68e602fc07ddacd7d512118ba7a2d1e
Nikita Venevitin <venevitinnikita@gmail.com>
2016-12-14 20:16:34 +0300

    Implemented main loop

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,40 @@ extern crate gl;
 extern crate sdl2;
 
 use std::ffi::CString;
+use std::time::{Instant, Duration};
 use gl::types::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
 const VERTEX_SHADER: &'static str = "\
-#version 150
+#version 130
 
 in vec2 position;
+in vec3 color;
+
+out vec3 Color;
 
 void main()
 {
+    Color = color;
     gl_Position = vec4(position, 0.0, 1.0);
 }
 ";
 
 const FRAGMENT_SHADER: &'static str = "\
-#version 150
+#version 130
 
-out vec4 outColor;
+in vec3 Color;
+
+out vec4 out_color;
 
 void main()
 {
-    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    out_color = vec4(Color, 1.0);
 }
 ";
+
+const MS_PER_UPDATE: u32 = 15;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -50,9 +59,9 @@ fn main() {
         gl::BindVertexArray(vao);
 
         let mut vertices = vec![
-        0f32, 0.5f32,
-        0.5f32, -0.5f32,
-        -0.5f32, -0.5f32
+        0f32, 0.5f32, 1f32, 0f32, 0f32,
+        0.5f32, -0.5f32, 0f32, 1f32, 0f32,
+        -0.5f32, -0.5f32, 0f32, 0f32, 1f32
         ];
 
         let mut vbo: GLuint = 0;
@@ -81,21 +90,31 @@ fn main() {
         gl::AttachShader(shader_program, vertex_shader);
         gl::AttachShader(shader_program, fragment_shader);
 
-        let out_color_str = CString::new("outColor").unwrap();
+        let out_color_str = CString::new("out_color").unwrap();
         gl::BindFragDataLocation(shader_program, 0, out_color_str.as_ptr());
 
         gl::LinkProgram(shader_program);
         gl::UseProgram(shader_program);
 
         let position_str = CString::new("position").unwrap();
-        let position_attr: GLuint = gl::GetAttribLocation(
-            shader_program, position_str.as_ptr()) as GLuint;
-        gl::VertexAttribPointer(position_attr, 2, gl::FLOAT, gl::FALSE, 0, 0 as *const _);
+        let position_attr = gl::GetAttribLocation(shader_program, position_str.as_ptr()) as u32;
+        gl::VertexAttribPointer(position_attr, 2, gl::FLOAT, gl::FALSE, 5 * 4, 0 as *const _);
         gl::EnableVertexAttribArray(position_attr);
+
+        let color_str = CString::new("color").unwrap();
+        let color_attr = gl::GetAttribLocation(shader_program, color_str.as_ptr()) as u32;
+        gl::VertexAttribPointer(color_attr, 3, gl::FLOAT, gl::FALSE, 5 * 4, (2 * 4) as *const _);
+        gl::EnableVertexAttribArray(color_attr);
     }
 
+    let ms_per_update = Duration::from_millis(MS_PER_UPDATE as u64);
+    let mut now = Instant::now();
+    let mut lag = Duration::from_millis(0);
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
+        lag += now.elapsed();
+        now = Instant::now();
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
@@ -104,6 +123,16 @@ fn main() {
                 _ => {}
             }
         }
+
+        println!("Before update");
+        while lag >= ms_per_update {
+            // TODO update();
+            println!("{:?}", lag);
+            lag -= ms_per_update;
+        }
+        println!("After update");
+
+        // TODO render(lag / ms_per_update);
 
         unsafe { gl::DrawArrays(gl::TRIANGLES, 0, 3); }
         window.gl_swap_window();
